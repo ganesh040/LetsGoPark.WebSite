dde369009c4d805835eaafb320c1ce11e04d7a36
shatakshi <spurankar@seattleu.edu>
2023-05-24 16:53:32 -0700

    added comments with /// for classes and methods //for attibutes for ProductController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,23 +7,45 @@ using LetsGoPark.WebSite.Services;
 
 namespace LetsGoPark.WebSite.Controllers
 {
+    /// <summary>
+     /// API controller for managing products.
+     /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Constructor for the ProductsController class.
+        /// </summary>
+        /// <param name="productService">An instance of the JsonFileProductService.</param>
+        
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        /// <summary>
+        /// Property that holds the JsonFileProductService instance.
+        /// </summary>
+        
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// HTTP GET method for retrieving a collection of ProductModel objects.
+        /// </summary>
+        /// <returns>A collection of ProductModel objects.</returns>
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetAllData();
         }
 
+        
+        /// <summary>
+        /// HTTP PATCH method for updating the rating of a product.
+        /// </summary>
+        /// <param name="request">The rating request containing the product ID and new rating.</param>
+        /// <returns>An HTTP action result indicating the success of the operation.</returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,9 +54,13 @@ namespace LetsGoPark.WebSite.Controllers
             return Ok();
         }
 
+        // Class representing the request model for updating the rating of a product.
         public class RatingRequest
         {
+            // Property for storing the product ID.
             public string ProductId { get; set; }
+
+            // Property for storing the new rating value.
             public int Rating { get; set; }
         }
     }
