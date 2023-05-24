0c20427194d5b86b99b2f82dad220c4a1e0d800b
ganesh040 <129236476+ganesh040@users.noreply.github.com>
2023-05-23 17:36:04 -0700

    updated productmodel.cs to include producttype in it

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -13,20 +13,23 @@ namespace LetsGoPark.WebSite.Models
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
+        // Enum product type
+        public string ProductType { get; set; }
+
         public string Url { get; set; }
-        
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+
+        [StringLength(maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
         public string Description { get; set; }
 
         public int[] Ratings { get; set; }
 
-        public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
+
 
         public string Quantity { get; set; }
 
-        [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Price { get; set; }
 
         // Store the Comments entered by the users on this product
