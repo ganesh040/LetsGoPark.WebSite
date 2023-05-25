fd771b3a4e3600e30267b2105cd86475d2fd4fa7
shatakshi <spurankar@seattleu.edu>
2023-05-24 17:25:45 -0700

    added comments with /// for classes and methods //for attibutes in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,10 +8,19 @@ using LetsGoPark.WebSite.Services;
 
 namespace LetsGoPark.WebSite.Pages
 {
+    /// <summary>
+    /// PageModel class for the index page.
+    /// </summary>
     public class IndexModel : PageModel
     {
+
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// Constructor for the IndexModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance to use.</param>
+        /// <param name="productService">The JsonFileProductService instance to use.</param>
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
@@ -19,9 +28,15 @@ namespace LetsGoPark.WebSite.Pages
             ProductService = productService;
         }
 
+        // The JsonFileProductService instance for handling product-related operations.
         public JsonFileProductService ProductService { get; }
+        // The collection of products to be displayed on the index page
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        /// <summary>
+        /// Handler for the HTTP GET request.
+        /// Retrieves all products and assigns them to the Products property.
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetAllData();
