dac909190c5999021ae426b30109cd1775d6c199
shatakshi <spurankar@seattleu.edu>
2023-05-24 17:15:02 -0700

    added comments with /// for classes and methods //for attibutes in Read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -12,13 +12,26 @@ namespace LetsGoPark.WebSite.Pages.Product
         /// Data middletier
         /// </summary>
         public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Constructor for the ReadModel class.
+        /// </summary>
+        /// <param name="productService">An instance of the JsonFileProductService.</param>
         public ReadModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
         public ProductModel Product;
+
+        /// <summary>
+        /// Handler for the HTTP GET request.
+        /// Retrieves the specified product and assigns it to the Product property.
+        /// </summary>
+        /// <param name="id">The ID of the product to be retrieved.</param>
         public void OnGet(string id)
         {
+            // Retrieve the product with the specified ID from the ProductService.
+            // Assign it to the Product property.
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
         }
     }
