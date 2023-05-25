469d6f7a17b65581f6f0e630d2e2569428e48bdf
shatakshi <spurankar@seattleu.edu>
2023-05-24 17:30:05 -0700

    added comments with /// for classes and methods //for attibutes in Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace LetsGoPark.WebSite.Pages
 {
+    /// <summary>
+    /// PageModel class for the privacy page.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Constructor for the PrivacyModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance to use.</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Constructor for the PrivacyModel class.
+        /// </summary>
+        /// <param name="logger">The logger instance to use.</param>
         public void OnGet()
         {
         }
