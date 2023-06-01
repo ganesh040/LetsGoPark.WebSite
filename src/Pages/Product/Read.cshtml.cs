1a4dcf61c1bb6d1150c213b3eb47424bad5e1131
ganesh040 <129236476+ganesh040@users.noreply.github.com>
2023-05-31 21:12:49 -0700

    modified real model file so accroding to the read.cshtml in read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -1,38 +1,61 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 using LetsGoPark.WebSite.Models;
 using LetsGoPark.WebSite.Services;
-using Microsoft.AspNetCore.Mvc;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LetsGoPark.WebSite.Pages.Product
 {
+    /// <summary>
+    /// The read model. This is the model for the read page. 
+    /// It is responsible for getting the data from the JSON file and displaying it on the page. 
+    /// </summary>
     public class ReadModel : PageModel
     {
-        /// <summary>
-        /// Data middletier
-        /// </summary>
+        // The product service. This is used to get the data from the JSON file.
         public JsonFileProductService ProductService { get; }
 
         /// <summary>
-        /// Constructor for the ReadModel class.
-        /// </summary>
-        /// <param name="productService">An instance of the JsonFileProductService.</param>
+        /// Used to initialize the product service.
+        /// <summary>
         public ReadModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
+
         public ProductModel Product;
 
         /// <summary>
-        /// Handler for the HTTP GET request.
-        /// Retrieves the specified product and assigns it to the Product property.
-        /// </summary>
-        /// <param name="id">The ID of the product to be retrieved.</param>
-        public void OnGet(string id)
+        /// Define a method that will be called when the page is loaded with an id parameter
+        /// <summary>
+        public IActionResult OnGet(string id)
         {
-            // Retrieve the product with the specified ID from the ProductService.
-            // Assign it to the Product property.
+
             Product = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+            if (Product == null)
+            {
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+
+        /// <summary>
+        /// This method is used to get the average rating of the product. 
+        /// <summary>
+        public object GetAverageRating(string productId)
+        {
+
+            var products = ProductService.GetAllData();
+            var ratings = products.First(x => x.Id == productId).Ratings;
+            double average = 0;
+            if (ratings != null)
+            {
+                average = ratings.Average();
+            }
+            return average;
         }
     }
 }
