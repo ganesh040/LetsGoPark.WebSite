57e90dbbd797ca9100e9cc45db5ad4cc934c52ab
ganesh040 <129236476+ganesh040@users.noreply.github.com>
2023-05-31 21:15:08 -0700

    modified test cases for the read file in read.cshtml.tests.cs

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿using System.Linq;
 using NUnit.Framework;
 
 using LetsGoPark.WebSite.Pages.Product;
@@ -40,5 +39,71 @@ namespace UnitTests.Pages.Product.Read
             Assert.AreEqual("LAKE SAMMAMISH STATE PARK", pageModel.Product.Title);
         }
         #endregion OnGet
+
+        #region ongetnull
+        /// <summary>
+        /// The Unit test case to check if the location is null then  then in
+        /// should return to the index page
+        /// </summary>
+        [Test]
+        public void OnGet_With_Null_Location_Should_Redirect_To_Index()
+        {
+            // Arrange
+            var id = "letsgopark";
+
+            // Act
+            var result = pageModel.OnGet(id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(null, pageModel.Product);
+        }
+
+        #endregion ongetnull
+
+
+
+        #region GetAverageRating
+
+        /// <summary>
+        /// The Unit test case to check if average rating computation is correct
+        /// </summary> 
+        [Test]
+        public void GetAverageRating_Ratings_Is_Null_Should_Return_Zero()
+        {
+            // Arrange
+            var product = TestHelper.ProductService.GetAllData().First();
+
+            // Act
+            var result = pageModel.GetAverageRating(product.Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void GetAverageRating_Ratings_Not_Null_Should_Return_The_Avg_Rating_Of_Product_Matching_Product_Id()
+        {
+            // Arrange
+            var product = TestHelper.ProductService.GetAllData().First();
+
+            int numRatings = 5;
+
+            for (int i = 0; i < numRatings; i++)
+            {
+                TestHelper.ProductService.AddRating(product.Id, i + 1);
+            }
+
+            var average = TestHelper.ProductService.GetAllData().First().Ratings.Average();
+
+            // Act
+            var result = pageModel.GetAverageRating(product.Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(average, result);
+        }
+        #endregion GetAverageRating
     }
 }
